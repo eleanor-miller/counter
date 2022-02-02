99e9c4bf85f85ac2f8b5e597942fdc6315cd91e0
eleanor-miller <eleanormiller08@icloud.com>
2022-02-02 17:34:35 -0500

    Copy

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import copyright from "./copyright.svg";
+
+function Footer() {
+  return (
+    <div className="App-footer">
+      <img src={copyright} />
+    </div>
+  );
+}
+
+export default Footer;
