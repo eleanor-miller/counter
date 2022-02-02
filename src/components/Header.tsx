d6c967cca5f3c29ce1977aafbaba71bf45c9175e
eleanor-miller <eleanormiller08@icloud.com>
2022-02-02 17:34:54 -0500

    Copy

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./yarn.svg";
+
+function Header() {
+  return (
+    <div className="App-header">
+      <div>
+        <img src={logo} height={55} width={55} />
+      </div>
+      <div>
+        <h1>Hi, Eleanor</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
