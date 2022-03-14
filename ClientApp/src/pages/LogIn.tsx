a97d9382278650035032f8f0ba29318663b6a664
eleanor-miller <eleanormiller08@icloud.com>
2022-03-13 21:26:15 -0400

    rework project page

--- ClientApp/src/pages/LogIn.tsx
+++ ClientApp/src/pages/LogIn.tsx
@@ -1,85 +1,86 @@
-import React from "react";
-import { useState } from "react";
+import React, {
+  ChangeEvent,
+  ChangeEventHandler,
+  Component,
+  useEffect,
+  useState,
+} from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
 import "../App.css";
-import Header from "../components/Header";
+
+import { User } from "../Types";
+import { attemptLogin } from "../services/LoginService";
+
+import {
+  SlCard,
+  SlButton,
+  SlInput,
+  SlSelect,
+} from "@shoelace-style/shoelace/dist/react";
+
 import Footer from "../components/Footer";
 import logo from "../images/yarn-logo.svg";
 
-function App() {
+type LoginProps = {
+  user: User | null;
+  setUser: (user: User) => void;
+};
+
+function Login(props: LoginProps) {
+  const [creds, setCreds] = useState({ username: "", password: "" });
+  const navigate = useNavigate();
+
+  function handleInputChange(e: ChangeEvent) {
+    const target = e.target as HTMLInputElement;
+    const { id, value } = target;
+
+    if (id == "username") setCreds({ ...creds, username: value });
+    if (id == "password") setCreds({ ...creds, password: value });
+  }
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    console.log(creds);
+    attemptLogin(creds.username, creds.password).then((u) => {
+      if (u) {
+        props.setUser(u!);
+        console.log("user", u);
+        navigate("/home");
+      }
+    });
+  }
+
   return (
-    <>
-      <header className="App-logo">
-        <img src={logo} height={200} width={200} />
-      </header>
-
-      <div>Create Login Page</div>
-
-      <footer className="App-footer">
-        <Footer />
-      </footer>
-    </>
+    <SlCard className="card-login">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <SlInput
+          type="email"
+          label="Email"
+          placeholder="you@example.com"
+          size="medium"
+          required
+        />
+        <br />
+        <br />
+        <SlInput
+          type="password"
+          label="Password"
+          placeholder="Password Toggle"
+          size="medium"
+          toggle-password
+        />
+        <br />
+        <br />
+        <SlButton className="orange" type="submit">
+          Submit
+        </SlButton>
+        <br />
+        <br />
+      </form>
+    </SlCard>
   );
 }
 
-export default App;
-
-type State = {
-  username: string;
-  password: string;
-  isButtonDisabled: boolean;
-  helperText: string;
-  isError: boolean;
-};
-
-const initialState: State = {
-  username: "",
-  password: "",
-  isButtonDisabled: true,
-  helperText: "",
-  isError: false,
-};
-
-type Action =
-  | { type: "setUsername"; payload: string }
-  | { type: "setPassword"; payload: string }
-  | { type: "setIsButtonDisabled"; payload: boolean }
-  | { type: "loginSuccess"; payload: string }
-  | { type: "loginFailed"; payload: string }
-  | { type: "setIsError"; payload: boolean };
-
-const reducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case "setUsername":
-      return {
-        ...state,
-        username: action.payload,
-      };
-    case "setPassword":
-      return {
-        ...state,
-        password: action.payload,
-      };
-    case "setIsButtonDisabled":
-      return {
-        ...state,
-        isButtonDisabled: action.payload,
-      };
-    case "loginSuccess":
-      return {
-        ...state,
-        helperText: action.payload,
-        isError: false,
-      };
-    case "loginFailed":
-      return {
-        ...state,
-        helperText: action.payload,
-        isError: true,
-      };
-    case "setIsError":
-      return {
-        ...state,
-        isError: action.payload,
-      };
-  }
-};
+export default Login;
